4178c1f331da39bd7a801acc917e27df00267351
Sathya Laufer <sathya@laufers.net>
2017-08-03 22:20:01 +0200

    InfluxDB node

---
+++ influxdb/MyNode.cpp
@@ -0,0 +1,115 @@
+/* Copyright 2013-2017 Sathya Laufer
+ *
+ * Homegear is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Homegear is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Homegear.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ */
+
+#include "MyNode.h"
+
+namespace MyNode
+{
+
+MyNode::MyNode(std::string path, std::string nodeNamespace, std::string type, const std::atomic_bool* frontendConnected) : Flows::INode(path, nodeNamespace, type, frontendConnected)
+{
+}
+
+MyNode::~MyNode()
+{
+}
+
+
+bool MyNode::init(Flows::PNodeInfo info)
+{
+	try
+	{
+		auto settingsIterator = info->info->structValue->find("measurement");
+		if(settingsIterator != info->info->structValue->end()) _measurement = settingsIterator->second->stringValue;
+
+		if(_measurement.empty()) return false;
+
+		return true;
+	}
+	catch(const std::exception& ex)
+	{
+		Flows::Output::printEx(__FILE__, __LINE__, __PRETTY_FUNCTION__, ex.what());
+	}
+	catch(...)
+	{
+		Flows::Output::printEx(__FILE__, __LINE__, __PRETTY_FUNCTION__);
+	}
+	return false;
+}
+
+bool MyNode::start()
+{
+	return true;
+}
+
+void MyNode::input(Flows::PNodeInfo info, uint32_t index, Flows::PVariable message)
+{
+	try
+	{
+		Flows::PVariable& input = message->structValue->at("payload");
+		if(input->type != Flows::VariableType::tBoolean && input->type != Flows::VariableType::tFloat && input->type != Flows::VariableType::tInteger && input->type != Flows::VariableType::tInteger64 && input->type != Flows::VariableType::tString && input->type != Flows::VariableType::tBase64)
+		{
+			Flows::PVariable encodedValue = std::make_shared<Flows::Variable>(Flows::VariableType::tString);
+			encodedValue->stringValue = _jsonEncoder.getString(input);
+			input = encodedValue;
+		}
+
+		std::string query = _measurement + " value=" + (input->type == Flows::VariableType::tString ? "\"" : "") + input->toString() + (input->type == Flows::VariableType::tString ? "\"" : "");
+		Flows::PArray parameters = std::make_shared<Flows::Array>();
+		parameters->reserve(2);
+		parameters->push_back(std::make_shared<Flows::Variable>(false));
+		parameters->push_back(std::make_shared<Flows::Variable>(query));
+
+		Flows::PVariable result = invoke("influxdbWrite", parameters);
+
+		if(input->type != Flows::VariableType::tFloat && input->type != Flows::VariableType::tInteger && input->type != Flows::VariableType::tInteger64)
+		{
+			parameters->at(0)->booleanValue = true;
+			result = invoke("influxdbWrite", parameters);
+			if(result && result->errorStruct) Flows::Output::printError("Error writing data to InfluxDB: " + result->structValue->at("faultString")->stringValue);
+		}
+		else if(_first)
+		{
+			_first = false;
+			parameters = std::make_shared<Flows::Array>();
+			parameters->push_back(std::make_shared<Flows::Variable>(_measurement));
+			result = invoke("influxdbCreateContinuousQuery", parameters);
+		}
+	}
+	catch(const std::exception& ex)
+	{
+		Flows::Output::printEx(__FILE__, __LINE__, __PRETTY_FUNCTION__, ex.what());
+	}
+	catch(...)
+	{
+		Flows::Output::printEx(__FILE__, __LINE__, __PRETTY_FUNCTION__);
+	}
+}
+
+}
