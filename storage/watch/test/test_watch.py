bf628ccd2b06bd26bef454d77df4628a35316e08
Florian Schwarz <florian4grunbach@hotmail.de>
2021-04-15 13:30:06 +0200

    using unittest, first tests for create

--- storage/watch/test/test_watch.py
+++ storage/watch/test/test_watch.py
@@ -1,103 +1,84 @@
 from homegear import Homegear
-import sys
+import unittest
 import time
 import os
 
 
-def setup():
-    nodeIds = hg.addNodesToFlow("Watch Unit test", "unit-test", testFlow)
+class CreateTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        global hg
+        hg = Homegear("/var/run/homegear/homegearIPC.sock")
 
-    if not nodeIds:
-        raise SystemError('Error =>  Could not create flow.')
+        global path
+        path = os.getcwd() + "/testingDirectory"
+        if not os.path.exists(path):
+            os.mkdir(path)
 
-    n1 = nodeIds["n1"]
-    n2 = nodeIds["n2"]
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(path)
 
-    if not hg.restartFlows():
-        raise SystemError("Error => Could not restart flows.")
-
-    while not hg.nodeBlueIsReady():
-        time.sleep(1)
-
-    return n1, n2
-
-
-def test_create_directory():
-    n1, n2 = setup()
-    os.mkdir((path + "/foo"))
-    time.sleep(1)
-    inputHistory = hg.getNodeVariable(n2, "inputHistory0")
-    print(inputHistory)
-    try:
-        assert len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}"
-        assert inputHistory[0][1]['payload'] == "Create", f"Payload is {inputHistory[0][1]['payload']}, but should be Create)"
-
-        hg.removeNodesFromFlow("Watch Unit test", "unit-test")
-        os.rmdir((path + "/foo"))
-        return 1
-    except AssertionError as e:
-        print(e)
-        hg.removeNodesFromFlow("Watch Unit test", "unit-test")
-        os.rmdir((path + "/foo"))
-        return -1
-
-
-def test_create_directories():
-    pass
-
-
-def test_create_file():
-    pass
-
-
-def test_create_files():
-    pass
-
-if len(sys.argv) > 1:
-    hg = Homegear(sys.argv[1])
-else:
-    hg = Homegear("/var/run/homegear/homegearIPC.sock")
-
-path = os.getcwd() + "/testingDirectory"
-if not os.path.exists(path):
-    os.mkdir(path)
-
-testFlow = [
-    {
-        "id": "n1",
-        "type": "watch",
-        "path": path,
-        "wires": [
-            [{"id": "n2", "port": 0}]
+    def setUp(self):
+        testFlow = [
+            {
+                "id": "n1",
+                "type": "watch",
+                "path": path,
+                "recursive": "false",
+                "wires": [
+                    [{"id": "n2", "port": 0}]
+                ]
+            },
+            {
+                "id": "n2",
+                "type": "unit-test-helper",
+                "inputs": 1
+            }
         ]
-    },
-    {
-        "id": "n2",
-        "type": "unit-test-helper",
-        "inputs": 1
-    }
-]
+        nodeIds = hg.addNodesToFlow("Watch Unit test", "unit-test", testFlow)
 
-print("testing watch node")
-print("test 1: creating directory")
-if test_create_directory() == 1:
-    print("test passed")
-else:
-    print("test failed")
+        if not nodeIds:
+            raise SystemError('Error =>  Could not create flow.')
 
-if test_create_directories() == 1:
-    print("test passed")
-else:
-    print("test failed")
+        global n1, n2
+        n1 = nodeIds["n1"]
+        n2 = nodeIds["n2"]
 
-if test_create_file() == 1:
-    print("test passed")
-else:
-    print("test failed")
+        if not hg.restartFlows():
+            raise SystemError("Error => Could not restart flows.")
 
-if test_create_files() == 1:
-    print("test passed")
-else:
-    print("test failed")
+        while not hg.nodeBlueIsReady():
+            time.sleep(1)
 
-os.rmdir(path)
+    def tearDown(self):
+        hg.removeNodesFromFlow("Watch Unit test", "unit-test")
+        if os.path.exists(path + "/foo"):
+            os.rmdir((path + "/foo"))
+        elif os.path.exists(path + "/foo.txt"):
+            os.remove(path + "/foo.txt")
+
+    def test_createDirectory(self):
+        os.mkdir((path + "/foo"))
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['payload'], "Create", f"Payload is {inputHistory[0][1]['payload']}, but should be Create)")
+
+    def test_createDirectories(self):
+        pass
+
+    def test_createFile(self):
+        file = open(path + "/foo.txt", "x")
+        file.close()
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['payload'], "Create", f"Payload is {inputHistory[0][1]['payload']}, but should be Create)")
+
+    def test_createFiles(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
