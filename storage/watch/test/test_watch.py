031c9b28309bebe3954cb25103796ad1ba754884
Florian Schwarz <florian4grunbach@hotmail.de>
2021-04-22 10:58:35 +0200

    adds tests to modify files and directories

--- storage/watch/test/test_watch.py
+++ storage/watch/test/test_watch.py
@@ -1,7 +1,11 @@
+import stat
+
 from homegear import Homegear
 import unittest
 import time
+import datetime
 import os
+import shutil
 
 
 class CreateNotRecursiveTestCase(unittest.TestCase):
@@ -563,6 +567,157 @@ class WriteFileTestCase(unittest.TestCase):
         inputHistory = hg.getNodeVariable(n2, "inputHistory0")
         self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
         self.assertEqual(inputHistory[0][1]['action'], "File opened for writing closed", f"Payload is '{inputHistory[0][1]['action']}', but should be 'File opened for writing closed'")
+
+
+class ModifyTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        global hg
+        hg = Homegear("/var/run/homegear/homegearIPC.sock")
+
+        global path
+        path = os.getcwd() + "/testingDirectory"
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(path)
+
+    def setUp(self):
+        testFlow = [
+            {
+                "id": "n1",
+                "type": "watch",
+                "path": path,
+                "recursive": "true",
+                "wires": [
+                    [{"id": "n2", "port": 0}]
+                ]
+            },
+            {
+                "id": "n2",
+                "type": "unit-test-helper",
+                "inputs": 1
+            }
+        ]
+        nodeIds = hg.addNodesToFlow("Watch Unit test", "unit-test", testFlow)
+
+        if not nodeIds:
+            raise SystemError('Error =>  Could not create flow.')
+
+        global n1, n2
+        n1 = nodeIds["n1"]
+        n2 = nodeIds["n2"]
+
+        if not hg.restartFlows():
+            raise SystemError("Error => Could not restart flows.")
+
+        while not hg.nodeBlueIsReady():
+            time.sleep(1)
+
+    def tearDown(self):
+        hg.removeNodesFromFlow("Watch Unit test", "unit-test")
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+
+    def test_renameDirectory(self):
+        os.mkdir(path + "/foo")
+        time.sleep(1)
+        os.rename(path + "/foo", path + "/bar")
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['action'], "Renamed directory/file in new directory", f"Payload is '{inputHistory[0][1]['action']}', but should be 'Renamed directory/file in new directory'")
+        self.assertEqual(inputHistory[1][1]['action'], "Renamed directory/file in old directory", f"Payload is '{inputHistory[1][1]['action']}', but should be 'Renamed directory/file in old directory'")
+
+    def test_renameDirectories(self):
+        directory = []
+        for i in range(10):
+            directory.append(path + "/foo" + str(i))
+            os.mkdir(directory[i])
+        time.sleep(1)
+        for i in range(10):
+            os.rename(directory[i], path + "/bar" + str(i))
+            time.sleep(1)
+            inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+            self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+            self.assertEqual(inputHistory[0][1]['action'], "Renamed directory/file in new directory", f"Payload is '{inputHistory[0][1]['action']}', but should be 'Renamed directory/file in new directory'")
+            self.assertEqual(inputHistory[1][1]['action'], "Renamed directory/file in old directory", f"Payload is '{inputHistory[1][1]['action']}', but should be 'Renamed directory/file in old directory'")
+
+    def test_renameFile(self):
+        file = open(path + "/foo.txt", "x")
+        file.close()
+        time.sleep(1)
+        os.rename(path + "/foo.txt", path + "/bar.txt")
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['action'], "Renamed directory/file in new directory", f"Payload is '{inputHistory[0][1]['action']}', but should be 'Renamed directory/file  in new directory'")
+        self.assertEqual(inputHistory[1][1]['action'], "Renamed directory/file in old directory", f"Payload is '{inputHistory[1][1]['action']}', but should be 'Renamed directory/file in old directory'")
+
+    def test_renameFiles(self):
+        files = []
+        for i in range(10):
+            files.append(path + "/foo" + str(i) + ".txt")
+            file = open(files[i], "x")
+            file.close()
+        time.sleep(1)
+        for i in range(10):
+            os.rename(files[i], path + "/bar" + str(i) + ".txt")
+            time.sleep(1)
+            inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+            self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+            self.assertEqual(inputHistory[0][1]['action'], "Renamed directory/file in new directory", f"Payload is '{inputHistory[0][1]['action']}', but should be 'Renamed directory/file in new directory'")
+            self.assertEqual(inputHistory[1][1]['action'], "Renamed directory/file in old directory", f"Payload is '{inputHistory[1][1]['action']}', but should be 'Renamed directory/file in old directory'")
+
+    def test_moveDirectory(self):
+        os.mkdir(path + "/foo")
+        os.mkdir(path + "/foo/foo")
+        os.mkdir(path + "/bar")
+        time.sleep(1)
+        shutil.move(path + "/foo/foo", path + "/bar")
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['action'], "Renamed directory/file in new directory", f"Payload is '{inputHistory[0][1]['action']}', but should be 'Renamed directory/file in new directory'")
+        self.assertEqual(inputHistory[1][1]['action'], "Renamed directory/file in old directory", f"Payload is '{inputHistory[1][1]['action']}', but should be 'Renamed directory/file in old directory'")
+
+    def test_moveFile(self):
+        os.mkdir(path + "/foo")
+        os.mkdir(path + "/bar")
+        file = open(path + "/foo/foo.txt", "x")
+        file.close()
+        time.sleep(1)
+        shutil.move(path + "/foo/foo.txt", path + "/bar")
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['action'], "Renamed directory/file in new directory", f"Payload is '{inputHistory[0][1]['action']}', but should be 'Renamed directory/file in new directory'")
+        self.assertEqual(inputHistory[1][1]['action'], "Renamed directory/file in old directory", f"Payload is '{inputHistory[1][1]['action']}', but should be 'Renamed directory/file in old directory'")
+
+    def test_modifyMetaDataTime(self):
+        file = open(path + "/foo.txt", "x")
+        file.close()
+        date = datetime.datetime(year=2017, month=4, day=2, hour=19, minute=5, second=42)
+        modTime = time.mktime(date.timetuple())
+        os.utime(path + "/foo.txt", (modTime, modTime))
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['action'], "File attribute change", f"Payload is '{inputHistory[0][1]['action']}', but should be 'File attribute change'")
+
+    def test_modifyMetaDataRights(self):
+        file = open(path + "/foo.txt", "x")
+        file.close()
+        os.chmod(path + "/foo.txt", stat.S_IREAD)
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['action'], "File attribute change", f"Payload is '{inputHistory[0][1]['action']}', but should be 'File attribute change'")
 
 
 if __name__ == '__main__':
