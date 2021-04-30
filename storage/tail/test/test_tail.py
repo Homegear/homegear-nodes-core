a948de9c9710c0afb072aa18987d2704f13e1593
Florian Schwarz <florian4grunbach@hotmail.de>
2021-04-30 14:26:15 +0200

    tail node: test for rotating log

--- storage/tail/test/test_tail.py
+++ storage/tail/test/test_tail.py
@@ -3,6 +3,9 @@ import unittest
 import sys
 import time
 import os
+from logging.handlers import TimedRotatingFileHandler
+import logging
+import datetime
 
 
 class WriteToEmptyFile(unittest.TestCase):
@@ -229,7 +232,7 @@ class AppendToFile(unittest.TestCase):
         self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
         self.assertEqual(inputHistory[0][1]['payload'], text, f"Payload is '{inputHistory[0][1]['payload']}', but should be '{text}'")
 
-    def test_apendMultipleLines(self):
+    def test_appendMultipleLines(self):
         text = "This is a test\nThe answer to life the universe and everything\n42\nWrite something here"
         lastLine = "This is the last line"
         file = open(filePath, "a")
@@ -240,6 +243,93 @@ class AppendToFile(unittest.TestCase):
         inputHistory = hg.getNodeVariable(n2, "inputHistory0")
         self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
         self.assertEqual(inputHistory[0][1]['payload'], lastLine, f"Payload is '{inputHistory[0][1]['payload']}', but should be '{lastLine}'")
+
+
+class RotateLog(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        global hg, path
+        if socketPath:
+            hg = Homegear(socketPath)
+        else:
+            hg = Homegear("/var/run/homegear/homegearIPC.sock")
+
+        path = os.getcwd() + "/testingDirectory"
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.rmdir(path)
+
+    def setUp(self):
+        global filePath
+        filePath = path + "/foo.log"
+        file = open(filePath, "x")
+        file.close()
+        testFlow = [
+            {
+                "id": "n1",
+                "type": "tail",
+                "path": filePath,
+                "wires": [
+                    [{"id": "n2", "port": 0}]
+                ]
+            },
+            {
+                "id": "n2",
+                "type": "unit-test-helper",
+                "inputs": 1
+            }
+        ]
+        nodeIds = hg.addNodesToFlow("Watch Unit test", "unit-test", testFlow)
+
+        if not nodeIds:
+            raise SystemError('Error =>  Could not create flow.')
+
+        global n1, n2
+        n1 = nodeIds["n1"]
+        n2 = nodeIds["n2"]
+
+        if not hg.restartFlows():
+            raise SystemError("Error => Could not restart flows.")
+
+        while not hg.nodeBlueIsReady():
+            time.sleep(1)
+
+        self.createTimedRotatingLog()
+        time.sleep(1)
+
+    def tearDown(self):
+        hg.removeNodesFromFlow("Watch Unit test", "unit-test")
+
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+
+    def createTimedRotatingLog(self):
+        global logger
+        logger = logging.getLogger("Rotating Log")
+        logger.setLevel(logging.INFO)
+        handler = TimedRotatingFileHandler(filePath, when="s", interval=5, backupCount=5)
+        logger.addHandler(handler)
+
+    def test_rotatingLog(self):
+        text = "The answer to life the universe and everything"
+        logger.info(text)
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['payload'], text, f"Payload is '{inputHistory[0][1]['payload']}', but should be '{text}'")
+        time.sleep(5)
+        text = "42"
+        logger.info(text)
+        time.sleep(1)
+        inputHistory = hg.getNodeVariable(n2, "inputHistory0")
+        self.assertTrue(len(inputHistory) >= 1, f"No message was passed on. Length is {len(inputHistory)}")
+        self.assertEqual(inputHistory[0][1]['payload'], text, f"Payload is '{inputHistory[0][1]['payload']}', but should be '{text}'")
 
 
 if __name__ == '__main__':
